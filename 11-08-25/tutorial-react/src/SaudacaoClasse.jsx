8c0246f515fd53645a7dc05ca6c877a6e637e6b6
dre-lnx <andrevlds23@gmail.com>
2025-08-11 22:45:42 -0300

    Fix: formato de reotnro saudacao

--- 11-08-25/tutorial-react/src/SaudacaoClasse.jsx
+++ 11-08-25/tutorial-react/src/SaudacaoClasse.jsx
@@ -1,10 +1,10 @@
 import { Component } from 'react';
+
 class SaudacaoClasse extends Component {
 render() {
-            <h1>
-                {this.props.isLogado &&
-                Hello, {this.props.name} - {this.props.age}}
-            </h1>
+        <h1>
+            Hello, {this.props.name} - {this.props.age}
+        </h1>
     }
 }
 export default SaudacaoClasse
